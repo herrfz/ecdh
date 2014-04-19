7c1d0fa32c5184e61d304a9a40a4e0823c80e461
Eriza Fazli <erizzaaaaa@gmail.com>
2014-04-19 16:58:26 +0200

    trying out multicast

--- test_client.py
+++ test_client.py
@@ -1,28 +1,39 @@
 # requires Python >3.2 due to more intuitive bytes handling
 import socket
+import struct
 from binascii import hexlify
 from ECDiffieHellman import ECDH
 
 HOST = 'localhost'          # The remote host
 PORT = 50008                # The same port as used by the server
+multicast_group = ('224.3.29.71', PORT)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.connect((HOST, PORT))
+sock.settimeout(1)
+ttl = struct.pack('b', 1)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+#sock.connect((HOST, PORT))
 
 dh = ECDH()
 ser_pub_key = b''.join([x.to_bytes(length=32, byteorder='big') 
     for x in dh.public_key])
 
-sock.send(ser_pub_key)
-data, server = sock.recvfrom(1024)
+try:
+    sent = sock.sendto(ser_pub_key, multicast_group)
 
-other_key = tuple([int.from_bytes(x, byteorder='big') 
-    for x in [data[:32], data[32:]]])
+    data, server = sock.recvfrom(1024)
 
-if dh.check_public_key(other_key):
-    dh.gen_secret(other_key)
-    print(hexlify(dh.gen_key()))
+    other_key = tuple([int.from_bytes(x, byteorder='big') 
+        for x in [data[:32], data[32:]]])
 
-else:
-    print('key error')
+    if dh.check_public_key(other_key):
+        dh.gen_secret(other_key)
+        print(hexlify(dh.gen_key()))
 
-sock.close()
+    else:
+        print('key error')
+
+except socket.timeout:
+    print('protocol timeout')
+
+finally:
+    sock.close()
--- test_server.py
+++ test_server.py
@@ -1,35 +1,46 @@
 # requires Python >3.2 due to more intuitive bytes handling
 import socket
+import struct
+import sys
 from binascii import hexlify
 from ECDiffieHellman import ECDH
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50008              # Arbitrary non-privileged port
+multicast_group = '224.3.29.71'
+server_address = (multicast_group, PORT)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((HOST, PORT))
+sock.bind(server_address)
+
+group = socket.inet_aton(multicast_group)
+mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 while True:
-    data, addr = sock.recvfrom(1024)
-    if not data: break
+    try:
+        data, addr = sock.recvfrom(1024)
+        if not data: break
 
-    other_key = tuple([int.from_bytes(x, byteorder='big') 
-        for x in [data[:32], data[32:]]])
+        other_key = tuple([int.from_bytes(x, byteorder='big') 
+            for x in [data[:32], data[32:]]])
 
-    dh = ECDH()
+        dh = ECDH()
 
-    if dh.check_public_key(other_key):
-        dh.gen_private_key()
-        dh.gen_public_key()
-        ser_pub_key = b''.join([x.to_bytes(length=32, byteorder='big') 
-            for x in dh.public_key])
+        if dh.check_public_key(other_key):
+            dh.gen_private_key()
+            dh.gen_public_key()
+            ser_pub_key = b''.join([x.to_bytes(length=32, byteorder='big') 
+                for x in dh.public_key])
 
-        sock.sendto(ser_pub_key, addr)
+            sock.sendto(ser_pub_key, addr)
 
-        dh.gen_secret(other_key)
-        print(hexlify(dh.gen_key()))
+            dh.gen_secret(other_key)
+            print(hexlify(dh.gen_key()))
 
-    else:
-        sock.sendto(bytes('key error', 'UTF-8'), addr)
-        print('key error')
+        else:
+            sock.sendto(bytes('key error', 'UTF-8'), addr)
+            print('key error')
 
-sock.close()
+    except KeyboardInterrupt:
+        sock.close()
+        sys.exit()
